c408ef04b4b2d654dd9af3ce40042858dba0fb81
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-09-25 12:09:00 +0100

    BLOC-169 Setup & configure unit testing

---
+++ run-tests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mkdir -p out
+
+javac -cp "out:lib/*" -d out $(find src/test/java -name "*.java")
+
+java -jar lib/junit-platform-console-standalone-1.9.3.jar \
+    --class-path out \
+    --scan-class-path
