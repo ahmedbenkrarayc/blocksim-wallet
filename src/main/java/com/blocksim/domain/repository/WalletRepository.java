b8a2e2bb6c0eafa0d115865a7df006cf350bd73f
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-09-28 01:41:03 +0100

    BLOC-127 created WalletRepository

---
+++ src/main/java/com/blocksim/domain/repository/WalletRepository.java
@@ -0,0 +1,14 @@
+package com.blocksim.domain.repository;
+
+import com.blocksim.domain.entity.Wallet;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface WalletRepository {
+    boolean save(Wallet wallet);
+    Optional<Wallet> findById(UUID id);
+    Optional<Wallet> findWalletByAddress(String address);
+    List<Wallet> findAll();
+}
