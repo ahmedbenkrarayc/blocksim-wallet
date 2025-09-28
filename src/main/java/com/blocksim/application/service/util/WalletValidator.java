400b4ada758ca3bbb8ff2bbdb5d1728fd2b0c170
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-09-29 00:05:57 +0100

    BLOC-128 Created WalletValidator util

---
+++ src/main/java/com/blocksim/application/service/util/WalletValidator.java
@@ -0,0 +1,13 @@
+package com.blocksim.application.service.util;
+
+public class WalletValidator {
+    public static void validateWalletType(String type) {
+        if (type == null || type.trim().isEmpty()) {
+            throw new IllegalArgumentException("Wallet type must not be empty");
+        }
+
+        if (!type.equalsIgnoreCase("bitcoin") && !type.equalsIgnoreCase("ethereum")) {
+            throw new IllegalArgumentException("Wallet type must either be bitcoin or ethereum");
+        }
+    }
+}
