9921e6baa7f119a4b3caccd7db17935edcdb031e
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-09-29 16:44:46 +0100

    BLOC-128 created wallet request dto

---
+++ src/main/java/com/blocksim/presentation/dto/request/WalletRequestDTO.java
@@ -0,0 +1,19 @@
+package com.blocksim.presentation.dto.request;
+
+public class WalletRequestDTO {
+    private String type;
+
+    public WalletRequestDTO() {}
+
+    public WalletRequestDTO(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
