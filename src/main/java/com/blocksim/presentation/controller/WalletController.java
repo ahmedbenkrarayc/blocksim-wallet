c68346d71756d828488dec3f0c0cfee0f06fb416
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-09-30 09:35:13 +0100

    BLOC-140 Implement createTransaction action in WalletController

--- src/main/java/com/blocksim/presentation/controller/WalletController.java
+++ src/main/java/com/blocksim/presentation/controller/WalletController.java
@@ -2,8 +2,11 @@ package com.blocksim.presentation.controller;
 
 import com.blocksim.application.service.WalletService;
 import com.blocksim.domain.entity.BitcoinWallet;
+import com.blocksim.domain.entity.Transaction;
 import com.blocksim.domain.entity.Wallet;
+import com.blocksim.presentation.dto.request.TransactionRequestDTO;
 import com.blocksim.presentation.dto.request.WalletRequestDTO;
+import com.blocksim.presentation.dto.response.TransactionResponseDTO;
 import com.blocksim.presentation.dto.response.WalletResponseDTO;
 
 public class WalletController {
@@ -21,6 +24,18 @@ public class WalletController {
                 wallet instanceof BitcoinWallet ? "bitcoin" : "ethereum",
                 wallet.getAddress(),
                 wallet.getBalance()
+        );
+    }
+
+    public TransactionResponseDTO createTransaction(TransactionRequestDTO requestDTO) {
+        Transaction transaction = walletService.createTransaction(requestDTO.getSourceAddress(), requestDTO.getDestinationAddress(), requestDTO.getAmount(), requestDTO.getPriority());
+
+        return new TransactionResponseDTO(
+                transaction.getId(),
+                transaction.getSourceAddress(),
+                transaction.getDestinationAddress(),
+                transaction.getAmount(),
+                transaction.getSourceAddress().startsWith("0x") ? "Ethereum" : "Bitcoin"
         );
     }
 }
