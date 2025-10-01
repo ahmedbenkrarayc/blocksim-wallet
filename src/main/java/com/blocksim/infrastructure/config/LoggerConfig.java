fd6456b588ce9d7a10a3eb0924973abe5083cc17
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-10-01 21:42:00 +0100

    Created LoggerConfig

---
+++ src/main/java/com/blocksim/infrastructure/config/LoggerConfig.java
@@ -0,0 +1,35 @@
+package com.blocksim.infrastructure.config;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.logging.*;
+
+public class LoggerConfig {
+
+    private static boolean initialized = false;
+    private final static String LOG_FILE = "logs/app.log";
+
+    public static void init() {
+        if (initialized) return;
+        initialized = true;
+
+        Logger rootLogger = Logger.getLogger("");
+        rootLogger.setLevel(Level.ALL);
+
+        try {
+            Files.createDirectories(Paths.get(LOG_FILE).getParent());
+
+            FileHandler fileHandler = new FileHandler(LOG_FILE, true);
+            fileHandler.setFormatter(new SimpleFormatter());
+            rootLogger.addHandler(fileHandler);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Logger getLogger(Class<?> clazz) {
+        return Logger.getLogger(clazz.getName());
+    }
+}
